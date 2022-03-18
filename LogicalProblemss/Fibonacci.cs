cc62b73fef74e1b46a2ff2d96fa900d828493432
ravikishorereddy <ravikishorereddy309@gmail.com>
2022-03-18 12:43:20 +0530

    Fibonacci series

---
+++ LogicalProblemss/Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblemss
+{
+    internal class Fibonacci
+    {
+        public void Fibo()
+        {
+            int num1 = 0, num2 = 1, num3, i, num;
+            Console.WriteLine("Enter the number:- ");
+            num = int.Parse(Console.ReadLine());
+            Console.Write(num1 + " " + num2 + " ");
+            for (i = 2; i < num; i++)
+            {
+                num3 = num1 + num2;
+                Console.Write(num3 + " ");
+                num1 = num2;
+                num2 = num3;
+            }
+        }
+    }
+}
---
+++ LogicalProblemss/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblemss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Logical Programs");
+            Console.WriteLine("Enter The Program Number to get Execute \n1.Fibonacci Series \n2.Perfect Number");
+            int Option;
+            Option = Convert.ToInt32(Console.ReadLine());
+             
+            switch(Option)
+            {
+                case 1:
+                    Fibonacci fibonacci = new Fibonacci();
+                    fibonacci.Fibo();
+                    break;
+                case 2:
+                    PerfectNumber perfectNumber = new PerfectNumber();
+                    perfectNumber.Perfect();
+                    break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
