3694d9fa79e3df9bbb989821c68ed806df20dfaa
ravikishorereddy <ravikishorereddy309@gmail.com>
2022-03-18 19:38:54 +0530

    PrimeNumber

--- LogicalProblemss/Program.cs
+++ LogicalProblemss/Program.cs
@@ -11,7 +11,8 @@ namespace LogicalProblemss
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Logical Programs");
-            Console.WriteLine("Enter The Program Number to get Execute \n1.Fibonacci Series \n2.Perfect Number");
+            Console.WriteLine("Enter The Program Number to get Execute \n1.Fibonacci Series \n2.Perfect Number" +
+               "\n3.PrimeNumber" );
             int Option;
             Option = Convert.ToInt32(Console.ReadLine());
              
@@ -24,6 +25,10 @@ namespace LogicalProblemss
                 case 2:
                     PerfectNumber perfectNumber = new PerfectNumber();
                     perfectNumber.Perfect();
+                    break;
+                case 3:
+                    PrimeNumber primeNumber = new PrimeNumber();
+                    primeNumber.Prime();
                     break;
                 default:
                     Console.WriteLine("Enter Valid Option");
